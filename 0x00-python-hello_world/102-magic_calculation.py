2236c1873bd5a2b83a2102a9d1407e620613a74b
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-02 15:30:58 -0700

    Hendy

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,8 @@
+def magic_calculation(a, b):
+    result = 98  # LOAD_CONST 1 (98)
+    result += a  # LOAD_FAST 0 (a)
+    result += b  # LOAD_FAST 1 (b)
+    result **= 2  # BINARY_POWER
+    result += a + b  # BINARY_ADD
+    return result  # RETURN_VALUE
+
