cec40be22e042278142dc6e482fa8a7067a845e8
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-03 18:22:44 -0700

    hendy

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for character in range(97,123):
-    if chr(character) != 'q' and chr(character) != 'e':
+for character in range(97, 123):
+    if chr(character) not in 'qe':
         print("{}".format(chr(character)), end="")
+
