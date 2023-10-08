47c57bf5b40c8a34671985cdc292944c6e55d1eb
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-08 14:10:39 -0700

    hendy

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# Function retrieves an element from the list
+
 def element_at(my_list, idx):
+    # Function retrieves an element from the list
+
     if  idx < 0 or idx >= len(my_list):
         return None
     else:
