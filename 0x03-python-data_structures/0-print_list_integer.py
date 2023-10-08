087ab719bc3d4bce7259eb56e9f2501a06d206df
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-08 13:19:23 -0700

    hendy

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,8 +2,5 @@
 
 # Iterate through the list
 def print_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        print("Empty list")
-    else:
-        for i in my_list:
-            print("{:d}".format(i))
+    for i in my_list:
+        print("{:d}".format(i))
