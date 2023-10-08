8d240e8f1afacf6ab4c8c5322f60afeac6511366
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-08 16:16:12 -0700

    hendy

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-ddef print_reversed_list_integer(my_list=None):
+def print_reversed_list_integer(my_list=None):
     """Prints integers in reverse"""
     if my_list is None:
         my_list = []
