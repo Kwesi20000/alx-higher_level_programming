d7d8da665dd125f99fbb6f3b50f1227cabd5ad73
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-09 10:37:51 -0700

    hendy

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     if not sentence:  # Check if the sentence is empty
-        return (0, None)  # Return a tuple with length 0 and None as the first character
+        return (0, None)  # Return a tuple with length and character
     else:
-        return (len(sentence), sentence[0])  # Return a tuple with length and the first character
+        return (len(sentence), sentence[0])  
