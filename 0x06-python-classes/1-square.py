136c641c2c37ef28a855498973c6b15f0c2696e7
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-24 11:14:37 -0700

    hendy

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+"""Defines a square """
+
 
 class Square:
-    """
-    This is the Square class.
-    """
+    """Represents a square"""
 
     def __init__(self, size):
+        """Initializing this square class
+        Args: size - This represents the size of the square
         """
-        Initializes a new instance of the Square class.
 
-        Args:
-            size (int): The size of the square.
-
-        Note:
-            This constructor doesn't perform type or value verification.
-        """
         self.__size = size
