d81bef4b1016543823de8386366f2cf0c5d9ce25
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-08 15:37:06 -0800

    hendy

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-const lines = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing"
-];
-
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
 }
