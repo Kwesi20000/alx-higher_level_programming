7f84f37c3637a093da80e3945f6354a8336636b5
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-09 09:20:46 -0800

    hendy

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is 0 or not a positive integer
+      return Object.create(null);
+    }
+  }
+
+  print() {
+    // Print the rectangle using the character 'X'
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+};
