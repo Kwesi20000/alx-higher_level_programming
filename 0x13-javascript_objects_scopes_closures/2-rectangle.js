2e0fb8ff3a0596314b23c8490e0f63579d6227f2
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-09 09:19:04 -0800

    hendy

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      [this.width, this.height] = [w, h];
-    } else {
-      // Return an empty object if conditions are not met
-      return Object.create(null);
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 };
