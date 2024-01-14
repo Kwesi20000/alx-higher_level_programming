901a019792efaf603bec97a011b4eaf37be3144c
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-13 18:31:11 -0800

    hendy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script to list all State objects containing the letter 'a' from the database hbtn_0e_6_usa.
+Script to list all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa.
 """
 
 from sys import argv
