72579a288591ae498f1e13f11cca1988f571f8b0
Kwesi20000 <hendrickskumi@yahoo.com>
2024-02-20 09:53:14 -0800

    hendy

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-const apiUrl = process.argv[2];
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      const todos = JSON.parse(body);
-      const completedTasksByUser = {};
-
-      todos.forEach(todo => {
-        if (todo.completed) {
-          if (completedTasksByUser[todo.userId]) {
-            completedTasksByUser[todo.userId]++;
-          } else {
-            completedTasksByUser[todo.userId] = 1;
-          }
-        }
-      });
-
-      for (const userId in completedTasksByUser) {
-        console.log(`User ${userId} completed ${completedTasksByUser[userId]} tasks.`);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    } else {
-      console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-    }
+    });
+    console.log(completed);
   }
 });
