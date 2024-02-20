e36a80f78e7dd306cddad162c6aa3304e065474a
Kwesi20000 <hendrickskumi@yahoo.com>
2024-02-20 09:50:08 -0800

    hendy

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,24 +2,4 @@
 
 const fs = require('fs');
 const request = require('request');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      fs.writeFile(filePath, body, 'utf-8', (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(`Webpage content has been saved to ${filePath}`);
-        }
-      });
-    } else {
-      console.error(`Failed to fetch webpage. Status code: ${response.statusCode}`);
-    }
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
