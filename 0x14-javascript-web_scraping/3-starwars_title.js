d4bf90552b85f4ae4b8cfabc4d4069e951e630bb
Kwesi20000 <hendrickskumi@yahoo.com>
2024-02-20 09:46:29 -0800

    hendy

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    if (response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      console.log(movieData.title);
+    } else {
+      console.error(`Failed to fetch movie. Status code: ${response.statusCode}`);
+    }
+  }
+});
