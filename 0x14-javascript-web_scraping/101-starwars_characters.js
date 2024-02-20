aa7c3d28b56ecd11bb27d8af3e7643127c013e86
Kwesi20000 <hendrickskumi@yahoo.com>
2024-02-20 09:59:51 -0800

    hendy

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,43 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      const movieData = JSON.parse(body);
-      const charactersUrls = movieData.characters;
-      const charactersPromises = charactersUrls.map(characterUrl => {
-        return new Promise((resolve, reject) => {
-          request.get(characterUrl, (charError, charResponse, charBody) => {
-            if (charError) {
-              reject(charError);
-            } else {
-              if (charResponse.statusCode === 200) {
-                const characterData = JSON.parse(charBody);
-                resolve(characterData.name);
-              } else {
-                reject(`Failed to fetch character. Status code: ${charResponse.statusCode}`);
-              }
-            }
-          });
-        });
-      });
-
-      Promise.all(charactersPromises)
-        .then(names => {
-          names.forEach(name => console.log(name));
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    } else {
-      console.error(`Failed to fetch movie. Status code: ${response.statusCode}`);
-    }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
