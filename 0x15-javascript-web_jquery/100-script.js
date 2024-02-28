d3cf58320def65cd4bc3ce6fbe1af2250b18afbf
Kwesi20000 <hendrickskumi@yahoo.com>
2024-02-28 10:22:37 -0800

    hendy

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Wait for the DOM content to be loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the <header> element using document.querySelector
+    var header = document.querySelector('header');
+
+    // Update the text color to red
+    header.style.color = '#FF0000';
+});
